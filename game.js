aab5545a17a51a6e4d22b53f5a74bedb356e24ad
Cesar Lugo <cesarlugo00@hotmail.com>
2018-09-27 21:03:46 +0200

    several game methods

--- game.js
+++ game.js
@@ -5,12 +5,15 @@ function Game(parent) {
   self.gameElement = null;
   self.onGameOverCallBack = null;
 
+  self.__start();
+  self.__startLoop();
+
+
 }
-document.addEventListener('keyUp', self.handleKeyUp);
+document.addEventListener('keyup', self.handleKeyUp);
 
 Game.prototype.__start = function(){
   var self = this;
-
   self.gameElement = buildDom(`
   <main class="game container">
     <div class="game_canvas">
@@ -40,4 +43,118 @@ self.canvasElement.setAttribute('height', self.height);
 self.ctx = self.canvasElement.getContext('2d');
 }
 
+Game.prototype.__startLoop = function(){
+  var self = this;
 
+  self.score = 0;
+  self.enemies = [];
+  self.player = new Player(self.canvasElement);
+
+  // laser delay
+  if (!self.player.laserState){
+    self.handleKeyUp= function(evt){
+     if (evt.key === "a") { 
+       self.player.setlaserState(true);
+       // laser delay
+       laserTimer();
+      }
+     else { 
+       self.player.setlaserState(false);
+      } 
+    }
+  }
+
+  document.addEventListener("a",self.handleKeyUp);
+
+  function loop(){
+    self._clearAll();
+    self._updateAll();
+    self._renderAll();
+
+    if (self._isPlayerAlive()) {
+      requestAnimationFrame(loop);
+    }
+    else {
+      self.onGameOverCallBack();
+    }
+  }
+
+  requestAnimationFrame(loop);
+}
+
+Game.prototype._updateAll = function(){
+  var self = this;
+
+  self._spawnEnemy();
+
+  self._checkAllCollision();
+  self.enemies.forEach(function(item){
+    item.update();
+  })
+  self.player.update();
+  self._updateScoreBoard();
+  
+
+}
+
+Game.prototype._renderAll = function(){
+  var self = this;
+
+  self.enemies.forEach(function(item){
+    item.render();
+  })
+
+  if(self.player.laserState) {
+    self.player.render();
+  }
+}
+
+Game.prototype._clearAll = function(){
+  var self = this;
+
+  self.ctx.clearRect(0, 0, self.width, self.height);
+}
+
+Game.prototype._spawnEnemy = function(){
+  var self = this;
+
+  if (Math.random() > 0.95) {
+    var randomX = Math.random() * self.width * 0.9;
+    self.enemies.push(new Enemy(self.canvasElement, randomX, self.height));
+  }
+}
+
+Game.prototype._checkAllCollision = function(){
+  var self = this;
+
+  self.enemies.forEach(function(item,idx){
+    if (player.checkCollision(item)) {
+       self.enemies.splice(idx,1);
+       self.score++;
+    }
+  })
+}
+
+Game.prototype._isPlayerAlive = function(){
+  var self = this;
+  var gridBreached;
+  self.enemies.forEach(function(item){
+    if (item.x - item.size < self.player.x) {
+      gridBreached = true;
+    }
+  })
+   return gridBreached? true : false;
+}
+
+Game.prototype.updateScoreBoard = function(){
+    self.scoreElement.innerText = self.score;
+}
+
+Game.prototype.destroy = function(){
+   self.gameElement.remove();
+   document.removeEventListener("a", handleKeyUp);
+}
+
+Game.prototype.laserTimer = function(){
+   window.setTimeout(self.player.setlaserState(false),500)
+}
--- main.js
+++ main.js
@@ -7,34 +7,90 @@ function buildDom (html){
 }
 
 function main() {
-  //  var mainContainerElement = document.querySelector('#main-container');
+   var mainContainerElement = document.querySelector('#main-container');
 
-  // Splash place holders
-    var splashElement = null;
-    var splashButton = null;
+  //-- Splash 
 
-    var handleSplashClick = function (){
-      destroySplash();
-      buildGame();
-    }
+  //--place holders
+  var splashElement = null;
+  var splashButton = null;
 
-    function buildSplash(){
-      splashElement = buildDom(`
-        <main class="splash container">
-          <h1 class="splash_title">Defense Grid</h1>
-          <p>Instructions: Use your 'A' key to active the laser and defend the Earth from incoming asteroids</p>
-          <button>Start</button>
-        </main>
-      `)
+  var handleSplashClick = function (){
+    destroySplash();
+    buildGame();
+  }
 
-      mainContainerElement.appendChild(splashElement);
+  function buildSplash(){
+    splashElement = buildDom(`
+      <main class="splash container">
+        <h1 class="splash_title">Defense Grid</h1>
+        <p>Instructions: Use your 'A' key to active the laser and defend the Earth from incoming asteroids</p>
+        <button>Start</button>
+      </main>
+    `)
 
-      splashButton = document.querySelector('button');
-      splashButton.addEventListener('click', handleSplashClick);
-    }
+    mainContainerElement.appendChild(splashElement);
 
-    
+    splashButton = document.querySelector('button');
+    splashButton.addEventListener('click', handleSplashClick);
+  }
 
+  function destroySplash(){
+    splashButton.removeEventListener('click', handleSplashClick);
+    splashElement.remove();
+  }
 
+  //-- Game
 
+  //--placeholders
+
+  var game = null;
+
+  var handleGameover = function(){
+    destroyGame();
+    buildGameOver(game.score,game.userName);
+  }
+
+  function buildGame() {
+    game = new Game (mainContainerElement);
+    game.onOver = (handleGameover);
+  }
+
+  function destroyGame(){
+    game.destroy();
+  }
+
+  //-- Game Over
+
+   var gameOverElement = null;
+   var gameOverButton = null;
+
+   var handleGameover = function(){
+     destroyGameOver();
+     buildSplash();
+   }
+
+  function buildGameOver (score, userName){
+    gameOverElement = buildDom(`
+    <main class="gamever container">
+      <h1>Game Over</h1>
+      <p>Your score:<span class="score"></span></p>
+      <button>Play Again</button>
+    </main>
+    `)
+     mainContainerElement.appendChild(gameOverElement);
+
+     gameOverButton = document.querySelector('button');
+     gameOverButton.addEventListener('click', handleGameOverClick);
+
+     gameOverElement.remove();
+  }
+
+  function destroyGameOver(){
+    gameOverButton.removeEventListener('click', handleGameOverClick);
+    gameOverElement.remove();
+  }
+
+buildSplash();
 }
+document.addEventListener('DOMContentLoaded', main);
